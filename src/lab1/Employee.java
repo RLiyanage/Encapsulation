edded4171f6e72a44834eff6f5eced12c314bf13
Sanuth <Sanuth@10.30.17.98>
2017-01-30 23:50:48 -0600

    First Commit

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -4,14 +4,15 @@ import java.util.Date;
 
 /**
  * Fix the code in this class to do PROPERTY encapsulation correctly. Also
- * consider if any of the properties should be mandatory and use a 
- * constructor to enforce that. Review the tips in the document 
- * "EncapCheckList.pdf" if needed.
+ * consider if any of the properties should be mandatory and use a constructor
+ * to enforce that. Review the tips in the document "EncapCheckList.pdf" if
+ * needed.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.02
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.02
  */
 public class Employee {
+
     public final String COMPANY_NAME = "ACME";
     private String firstName;
     private String lastName;
@@ -22,9 +23,9 @@ public class Employee {
     private boolean movedIn;
     private String cubeId;
     private Date orientationDate;
-    
-    public Employee(){
-        
+
+    public Employee() {
+
     }
 
     public Date getOrientationDate() {
@@ -40,9 +41,10 @@ public class Employee {
     }
 
     public void setFirstName(String firstName) {
-        if (firstName == null ||firstName.isEmpty()){
-            System.out.println("Please enter name  again");
-        this.firstName = firstName;
+        if (firstName == null || firstName.isEmpty()) {
+            System.out.println("Please enter name again");
+        } else {
+            this.firstName = firstName;
         }
     }
 
@@ -51,23 +53,24 @@ public class Employee {
     }
 
     public void setLastName(String lastName) {
-        if (firstName == null ||firstName.isEmpty()){
+        if (firstName == null || firstName.isEmpty()) {
             System.out.println("Please enter name  again");
-        this.lastName = lastName;
+            this.lastName = lastName;
+        }
     }
-    }
+
     public String getSsn() {
         return ssn;
     }
 
     public void setSsn(String ssn) {
-        if (ssn == null ||ssn.length()!=9){
+        if (ssn == null || ssn.length() < 9) {
             System.out.println("Please enter ssn  again");
-        
-    }else{
-        this.ssn = ssn;
+        } else {
+            this.ssn = ssn;
         }
     }
+
     public String getCubeId() {
         return cubeId;
     }
@@ -107,7 +110,4 @@ public class Employee {
     public void setCubeId(String cubeId) {
         this.cubeId = cubeId;
     }
-
-
-
 }
